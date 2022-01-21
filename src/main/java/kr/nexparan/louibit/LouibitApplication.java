d5d82f6bf19bae0f59829a1b85a3b3814b7a4b5b
NexParan <vinchi.reo@gmail.com>
2022-01-21 12:07:13 +0900

    Initial commit

---
+++ src/main/java/kr/nexparan/louibit/LouibitApplication.java
@@ -0,0 +1,13 @@
+package kr.nexparan.louibit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouibitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouibitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/nexparan/louibit/LouibitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nexparan.louibit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouibitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
